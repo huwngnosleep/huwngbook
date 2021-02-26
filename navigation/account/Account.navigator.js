95870ee05859cf1c5be27b0dff3e7cf992edd6ca
huwngnosleep <thanhhung8210@gmail.com>
2021-02-26 15:31:23 +0700

    disable auth screen when not use app as guess

--- navigation/account/Account.navigator.js
+++ navigation/account/Account.navigator.js
@@ -7,14 +7,18 @@ import FriendsScreen from '../../screens/account/Friends.screen';
 import CreatePostScreen from '../../screens/home/CreatePost.screen';
 import AuthScreen from '../../screens/account/Auth.screen';
 import CreateUserScreen from '../../screens/account/CreateUser.screen';
+import { useSelector } from 'react-redux';
 
 const Stack = createStackNavigator()
 
 const AccountNavigator = () => {
+    const localId = useSelector((state) => state.auth.localId)
+    // localId !== null => current user is not guess => can not access to auth screen
+
     return (
         <Stack.Navigator>
             <Stack.Screen name="Account" component={AccountScreen} />
-            <Stack.Screen name="Authentication" component={AuthScreen} />
+            {localId === null ? <Stack.Screen name="Authentication" component={AuthScreen} /> : null}
             <Stack.Screen name="Create User" component={CreateUserScreen} />
             <Stack.Screen name="Profile" component={ProfileScreen} />
             <Stack.Screen name="Create Post" component={CreatePostScreen} />
